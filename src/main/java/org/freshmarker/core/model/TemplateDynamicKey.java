df99eb462fa50975d7434cfbb66731c0dfa317eb
Jens Kaiser <schegge42@gmail.com>
2023-05-11 06:47:04 +0200

    add falling ranges

--- src/main/java/org/freshmarker/core/model/TemplateDynamicKey.java
+++ src/main/java/org/freshmarker/core/model/TemplateDynamicKey.java
@@ -7,36 +7,41 @@ import org.freshmarker.core.model.primitive.TemplateString;
 
 public class TemplateDynamicKey implements TemplateExpression {
 
-  private final TemplateObject sequence;
-  private final TemplateObject dynamicKey;
+    private final TemplateObject sequence;
+    private final TemplateObject dynamicKey;
 
-  public TemplateDynamicKey(TemplateObject sequence, TemplateObject dynamicKey) {
-    this.sequence = sequence;
-    this.dynamicKey = dynamicKey;
-  }
+    public TemplateDynamicKey(TemplateObject sequence, TemplateObject dynamicKey) {
+        this.sequence = sequence;
+        this.dynamicKey = dynamicKey;
+    }
 
-  @Override
-  public TemplateObject evaluateToObject(ProcessContext context) {
-    TemplateNumber index = dynamicKey.evaluate(context, TemplateNumber.class);
-    TemplateObject templateObject = sequence.evaluateToObject(context);
-    int beginIndex = index.getValue().getNumber().intValue();
-    if (templateObject instanceof TemplateString templateString) {
-      String value = templateString.getValue();
-      return new  TemplateString(value.substring(beginIndex, beginIndex + 1));
+    @Override
+    public TemplateObject evaluateToObject(ProcessContext context) {
+        TemplateNumber index = dynamicKey.evaluate(context, TemplateNumber.class);
+        TemplateObject templateObject = sequence.evaluateToObject(context);
+        int beginIndex = index.getValue().getNumber().intValue();
+        if (templateObject instanceof TemplateString templateString) {
+            String value = templateString.getValue();
+            return new TemplateString(value.substring(beginIndex, beginIndex + 1));
+        }
+        if (templateObject instanceof TemplateRange range) {
+            TemplateNumber lower = range.getLower().evaluate(context, TemplateNumber.class);
+            if (range.isRightUnlimited()) {
+                return lower.add(index);
+            }
+            TemplateNumber upper = range.getUpper().evaluate(context, TemplateNumber.class);
+            if (Math.abs(lower.asInt() - upper.asInt()) >= index.asInt()) {
+                throw new ProcessException("index out of range: " + index);
+            }
+            TemplateNumber result;
+            if (lower.asInt() < upper.asInt()) {
+                result = lower.add(index);
+            } else {
+                result = lower.add(index.negate());
+            }
+            return result;
+        }
+        TemplateListSequence list = (TemplateListSequence) templateObject;
+        return list.get(context, beginIndex);
     }
-    if (templateObject instanceof TemplateRange range) {
-      TemplateNumber lower = range.getLower().evaluate(context, TemplateNumber.class);
-      TemplateNumber result = lower.add(index);
-      if (range.isRightUnlimited()) {
-        return result;
-      }
-      TemplateNumber upper = range.getUpper().evaluate(context, TemplateNumber.class);
-      if (result.compare(upper).getValue().getNumber().intValue() < 0) {
-        return result;
-      }
-      throw new ProcessException("index out of range: " + result.getValue() + " " + upper.getValue());
-    }
-    TemplateListSequence list = (TemplateListSequence) templateObject;
-    return list.get(context, beginIndex);
-  }
 }
