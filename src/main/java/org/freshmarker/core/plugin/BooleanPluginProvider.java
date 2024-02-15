8e8ffcb5e9beb2f2ec0b46f375b2aa66e462ad2a
Jens Kaiser <schegge42@gmail.com>
2024-02-15 21:32:41 +0100

    fix ci

--- src/main/java/org/freshmarker/core/plugin/BooleanPluginProvider.java
+++ src/main/java/org/freshmarker/core/plugin/BooleanPluginProvider.java
@@ -1,24 +1,25 @@
 package org.freshmarker.core.plugin;
 
-import java.util.Map;
 import org.freshmarker.core.ProcessContext;
-import org.freshmarker.core.buildin.BuiltInKey;
 import org.freshmarker.core.buildin.BuiltIn;
+import org.freshmarker.core.buildin.BuiltInKey;
 import org.freshmarker.core.buildin.BuiltInMethod;
 import org.freshmarker.core.model.TemplateObject;
 import org.freshmarker.core.model.primitive.TemplateBoolean;
 import org.freshmarker.core.model.primitive.TemplateString;
 
-public class BooleanPluginProvider implements PluginProvider {
-  @Override
-  public void registerBuildIn(Map<BuiltInKey, BuiltIn> builtIns) {
-    new MethodBuiltInHelper().registerBuiltIns(this, builtIns);
-  }
+import java.util.Map;
 
-  @BuiltInMethod("c")
-  public static TemplateString computerBuiltIn(TemplateBoolean value) {
-    return new TemplateString(String.valueOf(value));
-  }
+public class BooleanPluginProvider implements PluginProvider {
+    @Override
+    public void registerBuildIn(Map<BuiltInKey, BuiltIn> builtIns) {
+        new MethodBuiltInHelper().registerBuiltIns(this, builtIns);
+    }
+
+    @BuiltInMethod("c")
+    public static TemplateString computerBuiltIn(TemplateBoolean value) {
+        return new TemplateString(String.valueOf(value));
+    }
 
     @BuiltInMethod("then")
     public static TemplateObject thenBuildIn(TemplateBoolean value, ProcessContext context, TemplateObject trueValue, TemplateObject falseValue) {
