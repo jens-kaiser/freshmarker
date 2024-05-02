f9671610bbd9d5c3867e6f72ffbc7368260aceea
Jens Kaiser <schegge42@gmail.com>
2024-05-02 18:00:19 +0200

    simplify unit tests

--- src/test/java/org/freshmarker/core/ftl/FtlTemplateBuilderTest.java
+++ src/test/java/org/freshmarker/core/ftl/FtlTemplateBuilderTest.java
@@ -1,102 +1,91 @@
 package org.freshmarker.core.ftl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import ftl.ParseException;
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import org.freshmarker.core.StringTemplateLoader;
 import org.freshmarker.Configuration;
 import org.freshmarker.Template;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FtlTemplateBuilderTest {
 
-  private Configuration configuration;
-  private StringTemplateLoader templateLoader;
+    private Configuration configuration;
 
-  @BeforeEach
-  public void setUp() {
-    configuration = new Configuration();
-    configuration.setLocale(Locale.GERMANY);
-    templateLoader = new StringTemplateLoader();
-    configuration.registerTemplateLoader(templateLoader);
-  }
+    @BeforeEach
+    public void setUp() {
+        configuration = new Configuration();
+        configuration.setLocale(Locale.GERMANY);
+    }
 
-  @Test
-  void generateWithMap() throws IOException, ParseException {
-    templateLoader.putTemplate("test", "${bean.name}");
-    Template template = configuration.getTemplate("test");
-    assertEquals("Bean Name", template.process(Map.of("bean", Map.of("name", "Bean Name"))));
-  }
+    @Test
+    void generateWithMap() throws IOException, ParseException {
+        Template template = configuration.getTemplate("test", "${bean.name}");
+        assertEquals("Bean Name", template.process(Map.of("bean", Map.of("name", "Bean Name"))));
+    }
 
-  @Test
-  void generateOnlytext() throws IOException, ParseException {
-    templateLoader.putTemplate("test", "the lazy dog jumps over the quick brown fox");
-    Template template = configuration.getTemplate("test");
-    assertEquals("the lazy dog jumps over the quick brown fox", template.process(Map.of()));
-  }
+    @Test
+    void generateOnlytext() throws ParseException {
+        Template template = configuration.getTemplate("test", "the lazy dog jumps over the quick brown fox");
+        assertEquals("the lazy dog jumps over the quick brown fox", template.process(Map.of()));
+    }
 
-  @Test
-  void generateTextInterpolation() throws ParseException, IOException {
-    templateLoader.putTemplate("test", "test: ${'test'}");
-    Template template = configuration.getTemplate("test");
-    assertEquals("test: test", template.process(Map.of()));
-  }
+    @Test
+    void generateTextInterpolation() throws ParseException {
+        Template template = configuration.getTemplate("test", "test: ${'test'}");
+        assertEquals("test: test", template.process(Map.of()));
+    }
 
-  @Test
-  void generateHtmlInterpolation() throws ParseException, IOException {
-    templateLoader.putTemplate("test", "test: ${'<br/>'}");
-    configuration.setOutputFormat("HTML");
-    Template template = configuration.getTemplate("test");
-    assertEquals("test: &lt;br/&gt;", template.process(Map.of()));
-  }
+    @Test
+    void generateHtmlInterpolation() throws ParseException {
+        configuration.setOutputFormat("HTML");
+        Template template = configuration.getTemplate("test", "test: ${'<br/>'}");
+        assertEquals("test: &lt;br/&gt;", template.process(Map.of()));
+    }
 
-  @Test
-  void generateHtmlInterpolationEsc() throws ParseException, IOException {
-    templateLoader.putTemplate("test", "test: ${'<br/>'?esc('HTML')}");
-    Template template = configuration.getTemplate("test");
-    assertEquals("test: &lt;br/&gt;", template.process(Map.of()));
-  }
+    @Test
+    void generateHtmlInterpolationEsc() throws ParseException {
+        Template template = configuration.getTemplate("test", "test: ${'<br/>'?esc('HTML')}");
+        assertEquals("test: &lt;br/&gt;", template.process(Map.of()));
+    }
 
-  @Test
-  void generateHtmlInterpolationNoEsc() throws ParseException, IOException {
-    templateLoader.putTemplate("test", "test: ${'<br/>'?no_esc}");
-    configuration.setOutputFormat("HTML");
-    Template template = configuration.getTemplate("test");
-    assertEquals("test: <br/>", template.process(Map.of()));
-  }
+    @Test
+    void generateHtmlInterpolationNoEsc() throws ParseException {
+        configuration.setOutputFormat("HTML");
+        Template template = configuration.getTemplate("test", "test: ${'<br/>'?no_esc}");
+        assertEquals("test: <br/>", template.process(Map.of()));
+    }
 
-  @Test
-  void generateDirectives() throws ParseException, IOException {
-    templateLoader.putTemplate("test", "test: <#list 1..4 as s><#if s % 2 == 0>${s} is even<#else>${s} is odd</#if> </#list>");
-    Template template = configuration.getTemplate("test");
-    assertEquals("test: 1 is odd 2 is even 3 is odd 4 is even ", template.process(Map.of()));
-  }
+    @Test
+    void generateDirectives() throws ParseException {
+        Template template = configuration.getTemplate("test",
+                "test: <#list 1..4 as s><#if s % 2 == 0>${s} is even<#else>${s} is odd</#if> </#list>");
+        assertEquals("test: 1 is odd 2 is even 3 is odd 4 is even ", template.process(Map.of()));
+    }
 
-  @Test
-  void generateMultiListDirectives() throws ParseException, IOException {
-    templateLoader.putTemplate("test",
-        "<#list list as s with l>" +
-               "<#list 1..2 as s with l>" +
-               "<#if s % 2 == 0>${s} is even<#else>${s} is odd</#if>" +
-               "<#if l?has_next>, </#if>" +
-               "</#list>" +
-               "<#if l?is_last>.<#else>, </#if>" +
-               "</#list>");
-    Template template = configuration.getTemplate("test");
-    Map<String, Object> dataModel = Map.of("list", List.of(1,2,3,4));
-    assertEquals("1 is odd, 2 is even, 1 is odd, 2 is even, 1 is odd, 2 is even, 1 is odd, 2 is even.", template.process(
-        dataModel));
-  }
+    @Test
+    void generateMultiListDirectives() throws ParseException {
+        Template template = configuration.getTemplate("test", """
+                <#list list as s with l>\
+                <#list 1..2 as s with l>\
+                <#if s % 2 == 0>${s} is even<#else>${s} is odd</#if>\
+                <#if l?has_next>, </#if>\
+                </#list>\
+                <#if l?is_last>.<#else>, </#if>\
+                </#list>""");
+        Map<String, Object> dataModel = Map.of("list", List.of(1, 2, 3, 4));
+        assertEquals("1 is odd, 2 is even, 1 is odd, 2 is even, 1 is odd, 2 is even, 1 is odd, 2 is even.", template.process(
+                dataModel));
+    }
 
-  @Test
-  void generateSequenceInterpolation() throws ParseException, IOException {
-    templateLoader.putTemplate("test", "test: ${test[1]}");
-    Template template = configuration.getTemplate("test");
-    assertEquals("test: 2", template.process(Map.of("test", List.of(1, 2, 3, 4, 5))));
-  }
+    @Test
+    void generateSequenceInterpolation() throws ParseException {
+        Template template = configuration.getTemplate("test", "test: ${test[1]}");
+        assertEquals("test: 2", template.process(Map.of("test", List.of(1, 2, 3, 4, 5))));
+    }
 }
--- src/test/java/org/freshmarker/core/ftl/RecordInterpolationTest.java
+++ src/test/java/org/freshmarker/core/ftl/RecordInterpolationTest.java
@@ -1,45 +1,40 @@
 package org.freshmarker.core.ftl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import ftl.ParseException;
-import java.io.IOException;
-import java.util.Map;
 import org.freshmarker.Configuration;
 import org.freshmarker.Template;
 import org.freshmarker.core.ProcessException;
-import org.freshmarker.core.StringTemplateLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class RecordInterpolationTest {
 
-  private Configuration configuration;
-  private StringTemplateLoader templateLoader;
+    private Configuration configuration;
 
-  public record TestRecord(String name, boolean active) {
-  }
+    public record TestRecord(String name, boolean active) {
+    }
 
-  @BeforeEach
-  public void setUp() {
-    configuration = new Configuration();
-    templateLoader = new StringTemplateLoader();
-    configuration.registerTemplateLoader(templateLoader);
-  }
+    @BeforeEach
+    public void setUp() {
+        configuration = new Configuration();
+    }
 
-  @Test
-  void generateWithBean() throws IOException, ParseException {
-    templateLoader.putTemplate("test", "${record.name} ${record.active}");
-    Template template = configuration.getTemplate("test");
-    assertEquals("Record Name yes", template.process(Map.of("record", new TestRecord("Record Name", true))));
-  }
+    @Test
+    void generateWithBean() throws IOException, ParseException {
+        Template template = configuration.getTemplate("test", "${record.name} ${record.active}");
+        assertEquals("Record Name yes", template.process(Map.of("record", new TestRecord("Record Name", true))));
+    }
 
-  @Test
-  void generateWithUnknownBeanAttribute() throws IOException, ParseException {
-    templateLoader.putTemplate("test", "${record.value} ${record.active}");
-    Template template = configuration.getTemplate("test");
-    Map<String, Object> data = Map.of("record", new TestRecord("Record Name", true));
-    assertThrows(ProcessException.class, () -> template.process(data));
-  }
+    @Test
+    void generateWithUnknownBeanAttribute() throws IOException, ParseException {
+        Template template = configuration.getTemplate("test", "${record.value} ${record.active}");
+        Map<String, Object> data = Map.of("record", new TestRecord("Record Name", true));
+        assertThrows(ProcessException.class, () -> template.process(data));
+    }
 }
