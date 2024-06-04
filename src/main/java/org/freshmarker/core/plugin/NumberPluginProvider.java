e3a057c22bf97fcfd7e6360ecd707ab9a6fa85f9
Jens Kaiser <schegge42@gmail.com>
2024-06-04 13:41:54 +0000

    add cast plugins

--- src/main/java/org/freshmarker/core/plugin/NumberPluginProvider.java
+++ src/main/java/org/freshmarker/core/plugin/NumberPluginProvider.java
@@ -37,4 +37,34 @@ public class NumberPluginProvider implements PluginProvider {
     Formatter formatter = new Formatter(context.getEnvironment().getLocale());
     return new TemplateString(formatter.format(format.getValue(), value.getValue().getNumber()).toString());
   }
+
+  @BuiltInMethod("int")
+  public static TemplateNumber castInt(TemplateNumber value) {
+    return value.getValue().getType() == TemplateNumber.Type.INTEGER ? value : new TemplateNumber(value.getValue().getNumber().intValue());
+  }
+
+  @BuiltInMethod("long")
+  public static TemplateNumber castLong(TemplateNumber value) {
+    return value.getValue().getType() == TemplateNumber.Type.LONG ? value : new TemplateNumber(value.getValue().getNumber().longValue());
+  }
+
+  @BuiltInMethod("short")
+  public static TemplateNumber castShort(TemplateNumber value) {
+    return value.getValue().getType() == TemplateNumber.Type.SHORT ? value : new TemplateNumber(value.getValue().getNumber().shortValue());
+  }
+
+  @BuiltInMethod("byte")
+  public static TemplateNumber castByte(TemplateNumber value) {
+    return value.getValue().getType() == TemplateNumber.Type.BYTE ? value : new TemplateNumber(value.getValue().getNumber().byteValue());
+  }
+
+  @BuiltInMethod("double")
+  public static TemplateNumber castDouble(TemplateNumber value) {
+    return value.getValue().getType() == TemplateNumber.Type.DOUBLE ? value : new TemplateNumber(value.getValue().getNumber().doubleValue());
+  }
+
+  @BuiltInMethod("float")
+  public static TemplateNumber castFloat(TemplateNumber value) {
+    return value.getValue().getType() == TemplateNumber.Type.FLOAT ? value : new TemplateNumber(value.getValue().getNumber().floatValue());
+  }
 }
