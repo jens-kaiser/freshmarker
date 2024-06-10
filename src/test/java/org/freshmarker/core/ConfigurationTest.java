3eceb729b1966a915753144e95322ad948fda399
Jens Kaiser <schegge42@gmail.com>
2024-06-10 09:28:08 +0000

    add unit test

--- src/test/java/org/freshmarker/core/ConfigurationTest.java
+++ src/test/java/org/freshmarker/core/ConfigurationTest.java
@@ -5,6 +5,7 @@ import org.freshmarker.Configuration;
 import org.freshmarker.Template;
 import org.junit.jupiter.api.Test;
 
+import java.io.StringReader;
 import java.time.LocalTime;
 import java.util.Map;
 
@@ -17,6 +18,16 @@ class ConfigurationTest {
     void getStringTemplate() throws ParseException {
         Configuration configuration = new Configuration();
         Template template = configuration.getTemplate("test", "test: ${temporal}");
+        assertNotNull(template);
+        String result = template.process(Map.of("temporal", LocalTime.of(12, 30)));
+        assertEquals("test: 12:30:00", result);
+    }
+
+    
+    @Test
+    void getReaderTemplate() throws ParseException {
+        Configuration configuration = new Configuration();
+        Template template = configuration.getTemplate("test", new StringReader("test: ${temporal}"));
         assertNotNull(template);
         String result = template.process(Map.of("temporal", LocalTime.of(12, 30)));
         assertEquals("test: 12:30:00", result);
