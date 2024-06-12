db3af04dc9d8330a7ccaec114be1f9cbb754849b
Jens Kaiser <schegge42@gmail.com>
2024-06-12 09:11:58 +0000

    add unit tests

--- src/test/java/org/freshmarker/core/ftl/TemporalInterpolationTest.java
+++ src/test/java/org/freshmarker/core/ftl/TemporalInterpolationTest.java
@@ -167,7 +167,10 @@ class TemporalInterpolationTest {
     @CsvSource({
             "test: ${temporal},test: 1968-08-24 11:30:45 Z",
             "test: ${temporal?c},test: 1968-08-24T11:30:45Z",
-            "test: ${temporal?string('dd. MMMM yyyy hh:mm')},test: 24. August 1968 11:30"
+            "test: ${temporal?string('dd. MMMM yyyy hh:mm')},test: 24. August 1968 11:30",
+            "test: ${temporal?date_time},test: 1968-08-24 12:30:45",
+            "test: ${temporal?date},test: 1968-08-24",
+            "test: ${temporal?time},test: 12:30:45"
     })
     void interpolationInstant(String input, String expected) throws ParseException {
         Template template = configuration.getTemplate("test", input);
@@ -207,7 +210,7 @@ class TemporalInterpolationTest {
         String result = template.process(Map.of("temporal", LOCAL_DATE_TIME.atZone(ZoneId.of("Europe/London"))));
         assertEquals("test: Europe/London", result);
     }
-    
+
     @Test
     void interpolationInvalidAtZone() throws ParseException {
         assertThrows(ParseException.class,
