c44249602acf1de83db1c2fb838c9e26575986a4
Jens Kaiser <schegge42@gmail.com>
2024-07-14 12:29:09 +0200

    fix version information

--- src/test/java/org/freshmarker/core/ftl/BuiltInVariableTestIT.java
+++ src/test/java/org/freshmarker/core/ftl/BuiltInVariableTestIT.java
@@ -3,25 +3,19 @@ package org.freshmarker.core.ftl;
 import ftl.ParseException;
 import org.freshmarker.Configuration;
 import org.freshmarker.Template;
-import org.junit.jupiter.api.BeforeEach;
+import org.freshmarker.test.util.EnabledIfMavenBuild;
 import org.junit.jupiter.api.Test;
 
-import java.util.Locale;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BuiltInVariableTestIT {
-    private Configuration configuration;
-
-    @BeforeEach
-    void setUp() {
-        configuration = new Configuration();
-        configuration.setLocale(Locale.GERMANY);
-    }
 
     @Test
+    @EnabledIfMavenBuild
     void builtInVariables() throws ParseException {
+        Configuration configuration = new Configuration();
         Template template = configuration.getTemplate("test", "test: ${.version}");
         assertEquals("test: 1.0.3-SNAPSHOT", template.process(Map.of()));
     }
---
+++ src/test/java/org/freshmarker/test/util/EnabledIfMavenBuild.java
@@ -0,0 +1,14 @@
+package org.freshmarker.test.util;
+
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@EnabledIfSystemProperty(named = "projectArtifactId", matches = "freshmarker")
+public @interface EnabledIfMavenBuild {
+}
