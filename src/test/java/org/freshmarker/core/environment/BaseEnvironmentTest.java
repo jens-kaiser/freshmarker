5f15063c7e3bd262efe3c24637718acfb341adfe
Jens Kaiser <schegge42@gmail.com>
2024-07-24 18:40:52 +0200

    add unit test

---
+++ src/test/java/org/freshmarker/core/environment/BaseEnvironmentTest.java
@@ -0,0 +1,44 @@
+package org.freshmarker.core.environment;
+
+import org.freshmarker.core.model.TemplateNull;
+import org.freshmarker.core.model.primitive.TemplateString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BaseEnvironmentTest {
+    private BaseEnvironment environment;
+
+    @BeforeEach
+    void setUp() {
+        environment = new BaseEnvironment(Map.of(), List.of(), Map.of(), Map.of(), null, null);
+    }
+
+    @Test
+    void getNestedContent() {
+        assertEquals(Optional.empty(), environment.getNestedContent());
+    }
+
+    @Test
+    void checkVariable() {
+        assertFalse(environment.checkVariable("name"));
+    }
+
+    @Test
+    void createVariable() {
+        assertThrows(UnsupportedOperationException.class, () -> environment.createVariable("name", new TemplateString("value")));
+    }
+
+    @Test
+    void setVariable() {
+        assertThrows(UnsupportedOperationException.class, () -> environment.setVariable("name", new TemplateString("value")));
+    }
+}
