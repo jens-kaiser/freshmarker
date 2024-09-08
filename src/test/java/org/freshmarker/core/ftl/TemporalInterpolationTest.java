91ccf4617033e1d999562a9180401bb701620229
Jens Kaiser <schegge42@gmail.com>
2024-09-08 19:55:43 +0200

    remove some tests

--- src/test/java/org/freshmarker/core/ftl/TemporalInterpolationTest.java
+++ src/test/java/org/freshmarker/core/ftl/TemporalInterpolationTest.java
@@ -134,17 +134,11 @@ class TemporalInterpolationTest {
     @ParameterizedTest
     @CsvSource(value = {
             "de;P2Y4M1D;test: ${temporal};test: P2Y4M1D",
-            "de;P2Y4M1D;test: ${temporal?h};test: 2 Jahre, 4 Monate und 1 Tag",
             "de;P4M1D;test: ${temporal};test: P4M1D",
-            "de;P4M1D;test: ${temporal?h};test: 4 Monate und 1 Tag",
             "de;P2Y2D;test: ${temporal};test: P2Y2D",
-            "de;P2Y2D;test: ${temporal?h};test: 2 Jahre und 2 Tage",
             "en;P2Y4M1D;test: ${temporal};test: P2Y4M1D",
-            "en;P2Y4M1D;test: ${temporal?h};test: 2 years, 4 months and 1 day",
             "en;P4M1D;test: ${temporal};test: P4M1D",
-            "en;P4M1D;test: ${temporal?h};test: 4 months and 1 day",
             "en;P2Y2D;test: ${temporal};test: P2Y2D",
-            "en;P2Y2D;test: ${temporal?h};test: 2 years and 2 days",
     }, delimiterString = ";")
     void interpolationPeriod(Locale locale, Period period, String input, String expected) throws ParseException {
         Template template = templateBuilder.withLocale(locale).getTemplate("test", input);
