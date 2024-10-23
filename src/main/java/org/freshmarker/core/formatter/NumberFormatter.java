7042fd27e2061b1f26b707b6055a64f8827979dc
Jens Kaiser <schegge42@gmail.com>
2024-10-23 12:05:33 +0200

    clean up code

--- src/main/java/org/freshmarker/core/formatter/NumberFormatter.java
+++ src/main/java/org/freshmarker/core/formatter/NumberFormatter.java
@@ -1,23 +1,22 @@
 package org.freshmarker.core.formatter;
 
-import java.text.NumberFormat;
-import java.util.Locale;
 import org.freshmarker.core.model.TemplateObject;
 import org.freshmarker.core.model.primitive.TemplateNumber;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class NumberFormatter implements Formatter {
 
-  private final LocaleLocal<NumberFormat> numberFormat;
+    private final LocaleLocal<NumberFormat> numberFormat;
 
-  public NumberFormatter() {
-    this.numberFormat = LocaleLocal.withInitial(NumberFormat::getNumberInstance);
-  }
-
-  @Override
-  public String format(TemplateObject object, Locale locale) {
-    if (object instanceof TemplateNumber number) {
-      return numberFormat.get(locale).format(number.getValue());
+    public NumberFormatter() {
+        this.numberFormat = LocaleLocal.withInitial(NumberFormat::getNumberInstance);
     }
-    return "";
-  }
+
+    @Override
+    public String format(TemplateObject object, Locale locale) {
+        TemplateNumber number = (TemplateNumber) object;
+        return numberFormat.get(locale).format(number.getValue());
+    }
 }
