d83cb8469a690806312aef84f81a68e2f1ec523e
Jens Kaiser <schegge42@gmail.com>
2024-11-03 12:18:24 +0100

    add unit test

--- src/test/java/org/freshmarker/core/ftl/MacroTest.java
+++ src/test/java/org/freshmarker/core/ftl/MacroTest.java
@@ -42,6 +42,11 @@ class MacroTest {
     }
 
     @Test
+    void generateFunction() throws ParseException {
+        assertThrows(ParseException.class, () -> builder.getTemplate("test", "<#function entry count></#function"));
+    }
+
+    @Test
     void generateComplexMacro() throws ParseException {
         Template template = builder.getTemplate("test", "<#macro entry count><#list 1..count as v>${v} <#nested/>\n</#list></#macro><@entry count=3>test</@entry>");
         assertEquals("1 test\n2 test\n3 test\n", template.process(Map.of()));
