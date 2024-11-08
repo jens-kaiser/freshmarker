c9651f2dfc9ccb551010a2e84ecdb3eae60f14ac
Jens Kaiser <schegge42@gmail.com>
2024-11-08 07:27:24 +0000

    Update NumberFormatter.java

--- src/main/java/org/freshmarker/core/formatter/NumberFormatter.java
+++ src/main/java/org/freshmarker/core/formatter/NumberFormatter.java
@@ -1,10 +1,17 @@
 package org.freshmarker.core.formatter;
 
+import org.freshmarker.core.ProcessException;
+import org.freshmarker.core.formatter.Formatter;
+import org.freshmarker.core.formatter.LocaleLocal;
 import org.freshmarker.core.model.TemplateObject;
 import org.freshmarker.core.model.primitive.TemplateNumber;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class NumberFormatter implements Formatter {
 
@@ -17,6 +24,19 @@ public class NumberFormatter implements Formatter {
     @Override
     public String format(TemplateObject object, Locale locale) {
         TemplateNumber number = (TemplateNumber) object;
-        return numberFormat.get(locale).format(number.getValue());
+        final NumberFormat format = numberFormat.get(locale);
+        return switch (number.getValue()) {
+            case Short n -> format.format(n.doubleValue());
+            case Byte n -> format.format(n.doubleValue());
+            case Integer n -> format.format(n.doubleValue());
+            case Long n -> format.format(n.doubleValue());
+            case Float n -> format.format(n.doubleValue());
+            case Double n -> format.format(n.doubleValue());
+            case AtomicInteger n -> format.format(n.doubleValue());
+            case AtomicLong n -> format.format(n.doubleValue());
+            case BigInteger n -> format.format(n);
+            case BigDecimal n -> format.format(n);
+            default -> throw new ProcessException("Unexpected number type: " + number.getValue());
+        };
     }
 }
