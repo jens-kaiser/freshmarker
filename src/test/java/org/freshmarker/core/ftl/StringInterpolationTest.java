50ec5d342c0b7476150813355a0dfa729f6a253d
Jens Kaiser <schegge42@gmail.com>
2024-11-12 10:34:02 +0000

    Update StringInterpolationTest.java

--- src/test/java/org/freshmarker/core/ftl/StringInterpolationTest.java
+++ src/test/java/org/freshmarker/core/ftl/StringInterpolationTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Map;
-import java.util.ResourceBundle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
