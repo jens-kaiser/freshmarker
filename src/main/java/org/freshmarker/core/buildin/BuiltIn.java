35a49fc15f76f0ca45ec30a11f4d6768260745a3
Jens Kaiser <schegge42@gmail.com>
2024-11-16 23:55:27 +0100

    clean up code

--- src/main/java/org/freshmarker/core/buildin/BuiltIn.java
+++ src/main/java/org/freshmarker/core/buildin/BuiltIn.java
@@ -1,5 +1,10 @@
 package org.freshmarker.core.buildin;
 
-public interface BuiltIn extends BuiltInFunction {
+import org.freshmarker.core.ProcessContext;
+import org.freshmarker.core.model.TemplateObject;
 
+import java.util.List;
+
+public interface BuiltIn {
+    TemplateObject apply(TemplateObject value, List<TemplateObject> parameters, ProcessContext context);
 }
--- src/main/java/org/freshmarker/core/buildin/BuiltInFunction.java
+++
@@ -1,9 +0,0 @@
-package org.freshmarker.core.buildin;
-
-import java.util.List;
-import org.freshmarker.core.ProcessContext;
-import org.freshmarker.core.model.TemplateObject;
-
-public interface BuiltInFunction {
-  TemplateObject apply(TemplateObject value, List<TemplateObject> parameters, ProcessContext context);
-}
