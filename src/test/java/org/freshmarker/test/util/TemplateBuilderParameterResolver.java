b178936e81682a8c1bdb14a8132470091ce8cae1
Jens Kaiser <schegge42@gmail.com>
2025-01-14 20:49:03 +0100

    extract DefaultTemplateBuilder

--- src/test/java/org/freshmarker/test/util/TemplateBuilderParameterResolver.java
+++ src/test/java/org/freshmarker/test/util/TemplateBuilderParameterResolver.java
@@ -1,6 +1,8 @@
 package org.freshmarker.test.util;
 
 import org.freshmarker.Configuration;
+import org.freshmarker.Configuration.TemplateBuilder;
+import org.freshmarker.DefaultTemplateBuilder;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
@@ -11,7 +13,7 @@ import java.util.Locale;
 public class TemplateBuilderParameterResolver implements ParameterResolver {
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return Configuration.TemplateBuilder.class == parameterContext.getParameter().getType();
+        return DefaultTemplateBuilder.class == parameterContext.getParameter().getType() || TemplateBuilder.class == parameterContext.getParameter().getType();
     }
 
     @Override
