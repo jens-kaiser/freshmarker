f51a253608a67bb7fecb5872fbfdeb9912dcefee
Jens Kaiser <schegge42@gmail.com>
2025-01-17 10:35:06 +0100

    add unit tests

---
+++ src/test/java/org/freshmarker/core/features/SimpleFeatureSetTest.java
@@ -0,0 +1,52 @@
+package org.freshmarker.core.features;
+
+import org.freshmarker.core.IncludeDirectiveFeature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.BitSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleFeatureSetTest {
+    private enum DemoFeature implements TemplateFeature {
+        FIRST,
+        SECOND
+    }
+
+    private SimpleFeatureSet simpleFeatureSet;
+
+    @BeforeEach
+    void setUp() {
+        TemplateFeatures templateFeatures = new TemplateFeatures();
+        templateFeatures.addSwitches(DemoFeature.FIRST, true);
+        templateFeatures.addSwitches(DemoFeature.SECOND, false);
+        simpleFeatureSet = templateFeatures.create();
+    }
+
+    @Test
+    void isEnabled() {
+        assertTrue(simpleFeatureSet.isEnabled(DemoFeature.FIRST));
+        assertFalse(simpleFeatureSet.isEnabled(DemoFeature.SECOND));
+    }
+
+    @Test
+    void isDisabled() {
+        assertFalse(simpleFeatureSet.isDisabled(DemoFeature.FIRST));
+        assertTrue(simpleFeatureSet.isDisabled(DemoFeature.SECOND));
+    }
+
+    @Test
+    void with() {
+        SimpleFeatureSet derived = simpleFeatureSet.with(DemoFeature.SECOND);
+        assertTrue(simpleFeatureSet.isDisabled(DemoFeature.SECOND));
+        assertTrue(derived.isEnabled(DemoFeature.SECOND));
+    }
+
+    @Test
+    void without() {
+        SimpleFeatureSet derived = simpleFeatureSet.without(DemoFeature.FIRST);
+        assertTrue(simpleFeatureSet.isEnabled(DemoFeature.FIRST));
+        assertTrue(derived.isDisabled(DemoFeature.FIRST));
+    }
+}
