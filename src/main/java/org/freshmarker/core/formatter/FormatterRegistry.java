c568f1f7336c1e25c599000311fba3e9313de351
Jens Kaiser <schegge42@gmail.com>
2025-01-17 19:34:49 +0100

    fix some texts

--- src/main/java/org/freshmarker/core/formatter/FormatterRegistry.java
+++ src/main/java/org/freshmarker/core/formatter/FormatterRegistry.java
@@ -8,6 +8,7 @@ import org.freshmarker.core.model.temporal.TemplateLocalDateTime;
 import org.freshmarker.core.model.temporal.TemplateLocalTime;
 import org.freshmarker.core.model.temporal.TemplateZonedDateTime;
 
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,13 +22,20 @@ public record FormatterRegistry(Map<Class<? extends TemplateObject>, Formatter> 
     public void registerFormatter(String type, String pattern) {
         switch (type) {
             case "number" -> formatter.put(TemplateNumber.class, new NumberFormatter(pattern));
+            case "zoned-date-time", "date", "date-time", "time" ->  registerDateFormatter(type, pattern, ZoneId.systemDefault());
+            default -> throw new IllegalStateException("Unexpected value: " + type);
+        }
+    }
+
+    public void registerDateFormatter(String type, String pattern, ZoneId zoneId) {
+        switch (type) {
             case "zoned-date-time" -> {
                 formatter.put(TemplateZonedDateTime.class, new DateTimeFormatter(pattern));
-                formatter.put(TemplateInstant.class, new DateTimeFormatter(pattern, ZoneOffset.UTC));
+                formatter.put(TemplateInstant.class, new DateTimeFormatter(pattern, zoneId));
             }
             case "date-time" -> formatter.put(TemplateLocalDateTime.class, new DateTimeFormatter(pattern));
             case "date" -> formatter.put(TemplateLocalDate.class, new DateFormatter(pattern));
-            case "time" -> formatter.put(TemplateLocalTime.class, new TimeFormatter(pattern, ZoneOffset.UTC));
+            case "time" -> formatter.put(TemplateLocalTime.class, new TimeFormatter(pattern, zoneId));
             default -> throw new IllegalStateException("Unexpected value: " + type);
         }
     }
