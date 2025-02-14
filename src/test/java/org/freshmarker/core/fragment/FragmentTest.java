53944935445ac0f304cd01fe8b2487e52f859628
Jens Kaiser <schegge42@gmail.com>
2025-02-14 09:30:58 +0100

    add some unit tests

---
+++ src/test/java/org/freshmarker/core/fragment/FragmentTest.java
@@ -0,0 +1,28 @@
+package org.freshmarker.core.fragment;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class FragmentTest {
+    @Test
+    void equalConstantFragments() {
+        ConstantFragment constantFragment1 = new ConstantFragment("test");
+        ConstantFragment constantFragment2 = new ConstantFragment("test");
+        ConstantFragment constantFragment3 = new ConstantFragment("tset");
+        assertEquals(constantFragment1, constantFragment2);
+        assertNotEquals(constantFragment1, constantFragment3);
+    }
+
+    @Test
+    void equalBlockFragments() {
+        BlockFragment blockFragment1 = new BlockFragment(List.of(new ConstantFragment("test")));
+        BlockFragment blockFragment2 = new BlockFragment(List.of(new ConstantFragment("test")));
+        BlockFragment blockFragment3 = new BlockFragment(List.of(new ConstantFragment("tset")));
+        assertEquals(blockFragment1, blockFragment2);
+        assertNotEquals(blockFragment1, blockFragment3);
+    }
+}
