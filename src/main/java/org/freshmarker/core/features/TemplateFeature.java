2d4b10afeaaa4a5a9066b76c5eb84a72fb56342a
Jens Kaiser <schegge42@gmail.com>
2025-04-03 16:02:09 +0200

    Update file TemplateFeature.java

--- src/main/java/org/freshmarker/core/features/TemplateFeature.java
+++ src/main/java/org/freshmarker/core/features/TemplateFeature.java
@@ -1,6 +1,6 @@
 package org.freshmarker.core.features;
 
 @Deprecated(since = "1.8.0", forRemoval = true)
-public interface TemplateFeature extends org.freshmarker.api.extension.TemplateFeature {
+public interface TemplateFeature extends org.freshmarker.api.TemplateFeature {
 
 }
