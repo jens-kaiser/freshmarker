f171a7eb02e8094597ffd889664bdc632d4ace10
Jens Kaiser <schegge42@gmail.com>
2025-08-02 12:27:07 +0200

    new develop version, fix parsing of Integer.MIN_VALUE literal, add long literals

--- src/test/java/org/freshmarker/core/ftl/LiteralTest.java
+++ src/test/java/org/freshmarker/core/ftl/LiteralTest.java
@@ -6,9 +6,12 @@ import org.freshmarker.test.util.TemplateBuilderParameterResolver;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -60,11 +63,18 @@ class LiteralTest {
 
     @Nested
     class Numbers {
-        @Test
-        void invalidIntegerLiteral(TemplateBuilder builder) {
-            NumberFormatException exception = assertThrows(NumberFormatException.class, () -> builder.getTemplate("test", "${21474836470}"));
-            assertEquals("For input string: \"21474836470\"", exception.getMessage());
+        @ParameterizedTest
+        @ValueSource(longs = { Long.MIN_VALUE, Integer.MIN_VALUE - 1L, Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE, Integer.MAX_VALUE + 1L, Long.MAX_VALUE })
+        void interpolateIntegerOrLongLiteral(long number, TemplateBuilder templateBuilder) {
+            Template template = templateBuilder.getTemplate("test", "${"+ number + "}");
+            assertDoesNotThrow(() -> template.process(Map.of()));
         }
 
+        @ParameterizedTest
+        @ValueSource(longs = { Long.MIN_VALUE, Integer.MIN_VALUE - 1L, Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE, Integer.MAX_VALUE + 1L, Long.MAX_VALUE })
+        void interpolateLongLiteral(long number, TemplateBuilder templateBuilder) {
+            Template template = templateBuilder.getTemplate("test", "${"+ number + "L}");
+            assertDoesNotThrow(() -> template.process(Map.of()));
+        }
     }
 }
