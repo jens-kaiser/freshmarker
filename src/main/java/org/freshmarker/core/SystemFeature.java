a8309ec3216b90204aab96cf5fb2ecf4505ef7ec
Jens Kaiser <schegge42@gmail.com>
2025-08-09 19:15:41 +0200

    add SequencedCollections, Arrays, Sets and Collections to sequences

--- src/main/java/org/freshmarker/core/SystemFeature.java
+++ src/main/java/org/freshmarker/core/SystemFeature.java
@@ -10,7 +10,27 @@ public enum SystemFeature implements TemplateFeature {
      *<p>
      * The feature is disabled by default.
      */
-    LOCALE_SENSITIVE_STRING_COMPARE;
+    LOCALE_SENSITIVE_STRING_COMPARE,
+
+    /**
+     * Feature that enables Set instances as sequences.
+     * This does not affect sets that are recognized as `SequencedCollection`.
+     *<p>
+     * The default value is 'false', meaning that Set are not used as sequences
+     *<p>
+     * The feature is disabled by default.
+     */
+    SET_AS_SEQUENCE,
+
+    /**
+     * Feature that enables Collection instances as sequences.
+     * This does not affect collections that are recognized as `SequencedCollection`.
+     *<p>
+     * The default value is 'false', meaning that Collections are not used as sequences
+     *<p>
+     * The feature is disabled by default.
+     */
+    COLLECTION_AS_SEQUENCE;
 
     @Override
     public boolean isEnabledByDefault() {
