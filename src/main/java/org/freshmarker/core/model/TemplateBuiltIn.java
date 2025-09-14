0d864b216ba4c6915052d7076217923a890e380a
Jens Kaiser <schegge42@gmail.com>
2025-09-14 23:31:48 +0200

    add built-ins to partial expression reduction

--- src/main/java/org/freshmarker/core/model/TemplateBuiltIn.java
+++ src/main/java/org/freshmarker/core/model/TemplateBuiltIn.java
@@ -2,8 +2,10 @@ package org.freshmarker.core.model;
 
 import org.freshmarker.core.ProcessContext;
 import org.freshmarker.core.ProcessException;
+import org.freshmarker.core.ReduceContext;
 import org.freshmarker.core.UnsupportedBuiltInException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class TemplateBuiltIn implements TemplateExpression {
@@ -42,6 +44,29 @@ public final class TemplateBuiltIn implements TemplateExpression {
 
     @Override
     public <R> R accept(TemplateObjectVisitor<R> visitor) {
-        return visitor.visit(this);
+        return visitor.visit(this, name, expression, parameter);
+    }
+
+    @Override
+    public TemplateObject reduce(ReduceContext context) {
+        TemplateObject result = expression.reduce(context);
+        boolean allReduced = true;
+        List<TemplateObject> reducedParameters = new ArrayList<>();
+        for (TemplateObject p : parameter) {
+            TemplateObject reducedParameter = p.reduce(context);
+            if (reducedParameter == p) {
+                allReduced = false;
+            }
+            reducedParameters.add(reducedParameter);
+        }
+
+        try {
+            return context.getBuiltIn(result.getClass(), name).apply(result, reducedParameters, context);
+        } catch (RuntimeException e) {
+            if (result == expression && !allReduced) {
+                return this;
+            }
+            return new TemplateBuiltIn(name, result, reducedParameters, ignoreOptionalNull, ignoreNull);
+        }
     }
 }
--- src/main/java/org/freshmarker/core/model/builtin/HookedBuiltIn.java
+++ src/main/java/org/freshmarker/core/model/builtin/HookedBuiltIn.java
@@ -3,11 +3,14 @@ package org.freshmarker.core.model.builtin;
 import org.freshmarker.api.BuiltIn;
 import org.freshmarker.core.ProcessContext;
 import org.freshmarker.core.ProcessException;
+import org.freshmarker.core.ReduceContext;
 import org.freshmarker.core.UnsupportedBuiltInException;
 import org.freshmarker.core.buildin.BuiltInKey;
 import org.freshmarker.core.model.TemplateNull;
 import org.freshmarker.core.model.TemplateObject;
+import org.freshmarker.core.model.TemplateObjectVisitor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HookedBuiltIn implements TemplateObject {
@@ -44,5 +47,36 @@ public class HookedBuiltIn implements TemplateObject {
         } catch (RuntimeException e) {
             throw new ProcessException(e.getMessage(), e);
         }
+    }
+
+    @Override
+    public TemplateObject reduce(ReduceContext context) {
+        TemplateObject result = expression.reduce(context);
+        boolean allReduced = true;
+        List<TemplateObject> reducedParameters = new ArrayList<>();
+        for (TemplateObject p : parameter) {
+            TemplateObject reducedParameter = p.reduce(context);
+            if (reducedParameter == p) {
+                allReduced = false;
+            }
+            reducedParameters.add(reducedParameter);
+        }
+        if (builtInKey.getType() != result.getClass()) {
+            return allReduced ? new HookedBuiltIn(result, builtInKey, builtIn, reducedParameters, ignoreOptionalEmpty, ignoreNull) : this;
+        }
+
+        try {
+            return builtIn.apply(result, reducedParameters, context);
+        } catch (RuntimeException e) {
+            if (result == expression && !allReduced) {
+                return this;
+            }
+            return new HookedBuiltIn(result, builtInKey, builtIn, reducedParameters, ignoreOptionalEmpty, ignoreNull);
+        }
+    }
+
+    @Override
+    public <R> R accept(TemplateObjectVisitor<R> visitor) {
+        return visitor.visit(this, builtInKey.getName(), expression, parameter);
     }
 }
